1c2c8a103fef26d20760b48ee0f7fb4d2254aeef
Gene Carlson <kvngncrlsn@gmail.com>
2025-01-05 13:35:59 +0900

    Add warning about @listings to Util.pm

--- SBO-Lib/lib/SBO/Lib/Util.pm
+++ SBO-Lib/lib/SBO/Lib/Util.pm
@@ -113,8 +113,10 @@ C<GPG_VERIFY>, C<RSYNC_DEFAULT> and C<STRICT_UPGRADES>.
 
 =head2 @listings
 
-An array with blacklisted packages and optional dependency requests read in
-from C</etc/sbotools/sbotools.hints>.
+An array with blacklisted scripts and optional dependency requests read in
+from C</etc/sbotools/sbotools.hints>. Only C<read_hints()> should interact
+with C<@listings> directly; in other situations, make a copy (see e.g.
+C<@on_blacklist()>.)
 
 =cut
 
