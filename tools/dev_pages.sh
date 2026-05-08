1eae8640290b378162aa837003b9dbcecd1e4de1
J Pipkin <jacob.pipkin@icloud.com>
2026-05-08 10:38:39 -0600

    use POSIX sh with #!/bin/sh, fix integer/string comparison bug

--- tools/dev_pages.sh
+++ tools/dev_pages.sh
@@ -3,13 +3,13 @@
 # Generate SBO::Lib::*.3 pages. Pass --all to generate all
 # pages, or the names of one or more individual modules.
 
-if ! [[ -d "./man3" ]]; then
+if ! [ -d "./man3" ]; then
 	echo "you do not seem to be at the right place to run this."
 	echo "the man3/ directory should be under ."
 	exit 1
 fi
 
-if [[ -z $1 ]]; then
+if [ -z $1 ]; then
   echo "Usage: e.g., ./tools/dev_pages.sh Build Tree"
   echo "Usage:       ./tools/dev_pages.sh --all"
   exit 1
@@ -21,7 +21,7 @@ fi
 version=$(grep '^our $VERSION' SBO-Lib/lib/SBO/Lib.pm | grep -Eo '[0-9]+(\.[0-9RC@gita-f_]+){0,2}')
 datestring="$(ddate +"%{%A, %B %d%}, %Y YOLD%N - %H")"
 cd SBO-Lib/lib || exit
-if [[ $1 = '--all' ]]; then
+if [ $1 = '--all' ]; then
   for item in Build Download Help Info Pkgs Readme Repo Solibs Tree Util ; do
     pod2man -r "" -c "sbotools $version" -d "$datestring" SBO/Lib/$item.pm ../../man3/SBO::Lib::$item.3
   done
@@ -29,10 +29,10 @@ if [[ $1 = '--all' ]]; then
   exit
 fi
 
-while [[ -n $@ ]]; do
-  if [[ -f SBO/Lib/$1.pm ]]; then
+while [ -n "$(echo $@)" ]; do
+  if [ -f SBO/Lib/$1.pm ]; then
     pod2man -r "" -c "sbotools $version" -d "$datestring" SBO/Lib/$1.pm ../../man3/SBO::Lib::$1.3
-  elif [[ $1 -eq "Lib" && -f SBO/Lib.pm ]]; then
+  elif [ $1 = "Lib" -a -f SBO/Lib.pm ]; then
     pod2man -r "" -c "sbotools $version" -d "$datestring" SBO/Lib.pm ../../man3/SBO::Lib.3
   else
     echo "The file $1.pm does not exist. Skipping."
